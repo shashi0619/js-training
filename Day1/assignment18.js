ece3b963417151ae78786a74b562197dd48a7844
shashi <shashichintu68@gmail.com>
2024-05-13 13:36:34 +0530

    const  variable multiple time

---
+++ Day1/assignment18.js
@@ -0,0 +1,15 @@
+//WAP to show assign values to const  variable multiple time and print them after each assignment
+
+const_variable1 = 17;
+
+const_variable2 = "Hello,Shashi";
+
+const_variable3 = [1,2,3,4,5];
+
+
+console.log(const_variable1);
+console.log(const_variable2);
+console.log(const_variable3);
+
+
+
