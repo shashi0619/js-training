8fe9a12d93c26f6b880f7abb7903b8b42a1bd6ee
shashi <shashichintu68@gmail.com>
2024-05-13 13:33:15 +0530

    let variable

---
+++ Day1/assignment17.js
@@ -0,0 +1,12 @@
+//WAP to show assign values to let variable multiple time and print them after each assignment
+
+let_variable = 12;
+
+let_variable2 = "Hello,Shashi";
+
+let_variable3 = [1,2,3,4,5];
+
+console.log(let_variable);
+console.log(let_variable2);
+console.log(let_variable3);
+
