93dbbdd5480dbf9c416fad8c2885a270fd05314b
shashi <shashichintu68@gmail.com>
2024-05-13 13:40:29 +0530

    var  variable multiple time

---
+++ Day1/assignment19.js
@@ -0,0 +1,15 @@
+//WAP to show assign values to var  variable multiple time and print them after each assignment
+
+
+var_variable = 18;
+console.log(var_variable)
+
+var_variable2 = "Hello,Shashi";
+console.log(var_variable2)
+
+
+var_variable3 = [1,2,3,4,5]
+console.log(var_variable3)
+
+
+
