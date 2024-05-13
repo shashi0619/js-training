fb26462c6407fc93cc78e303882ef25601450da4
shashi <shashichintu68@gmail.com>
2024-05-13 17:29:33 +0530

    usage of let variable  issue

---
+++ Day1/assignment44.js
@@ -0,0 +1,15 @@
+//WAP to fix the  the usage of let variable  issue;  ( print ;  delcare ; print ; assign ; print)
+
+let x;
+
+
+console.log(x);
+
+
+x = 1;
+
+
+console.log(x);
+
+
+console.log(x);
