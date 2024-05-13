43c57e431d7337baad0c430457769fac21a8f7a0
shashi <shashichintu68@gmail.com>
2024-05-13 11:28:02 +0530

    first commit

---
+++ Day1/assignment2.js
@@ -0,0 +1,7 @@
+//Write a program (WAP) to name variables with different types
+
+integer_variable = 11
+float_variable = 7.7777
+String_variable = "hello shashi"
+Boolean_variable = true/false
+list_variable = [1,2,3]
