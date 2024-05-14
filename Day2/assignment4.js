410d6f10a13c517a04767c80066d96411473361d
shashi <shashichintu68@gmail.com>
2024-05-14 07:57:12 +0530

    declare variables  with undefined datatype

---
+++ Day2/assignment4.js
@@ -0,0 +1,17 @@
+//WAP to declare variables  with undefined datatype
+
+let undefined_variable;
+
+console.log(undefined_variable);
+
+var x ;
+
+console.log(x)
+
+
+const y = undefined ;
+
+console.log(y)
+
+
+
