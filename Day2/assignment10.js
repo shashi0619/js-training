11d9dc1d6b1c4c324059a9ea3e09ee16f15ed6e6
shashi <shashichintu68@gmail.com>
2024-05-14 08:19:38 +0530

    number data from your linkedIn profile

---
+++ Day2/assignment10.js
@@ -0,0 +1,13 @@
+//WAP identifying the number data from your linkedIn profile, declare variables and assign values. Print them profile
+
+let age = 30;
+let number_of_connections = 30;
+let number_of_posts = 50;
+let number_of_likes = 55;
+let number_of_comments = 50;
+
+console.log(age);
+console.log(number_of_connections);
+console.log(number_of_posts);
+console.log(number_of_likes);
+console.log(number_of_comments);
