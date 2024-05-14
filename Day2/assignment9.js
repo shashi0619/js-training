768facb5cdbe50eab5acd9e368aad67e9e14328a
shashi <shashichintu68@gmail.com>
2024-05-14 08:15:31 +0530

    the string data from your linkedIn profile

---
+++ Day2/assignment9.js
@@ -0,0 +1,18 @@
+//WAP identifying the string data from your linkedIn profile, declare variables and assign values. Print them profile
+
+let name = " shashi";
+email = "shashixxx@email.com";
+city = "hyd";
+connections = "10";
+profile_views = "19";
+user_name = "shashi22";
+password = "12345678";
+
+
+console.log(name);
+console.log(email);
+console.log(city);
+console.log(connections);
+console.log(profile_views);
+console.log(user_name);
+console.log(password);
