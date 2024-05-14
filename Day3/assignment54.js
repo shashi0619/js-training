5fb5a0b7c6453cbbbd0afc703c993b3668d3c257
shashi <shashichintu68@gmail.com>
2024-05-14 22:47:38 +0530

    div three numbers

---
+++ Day3/assignment54.js
@@ -0,0 +1,18 @@
+//WAP to div three numbers
+
+const readlineSync = require('readline-sync');
+
+
+let firstNumber = (readlineSync.question('Enter the first number: '));
+
+
+let secondNumber = (readlineSync.question('Enter the second number: '));
+
+
+let thirdNumber = (readlineSync.question('Enter the third number: '));
+
+
+let result = firstNumber / secondNumber / thirdNumber;
+
+
+console.log('The result of dividing', firstNumber + ',', secondNumber + ', and', thirdNumber + ' is:', result);
