f195e831f632ac3a64d805f5aa1799897b706bb3
shashi <shashichintu68@gmail.com>
2024-05-14 18:08:30 +0530

    mul two positive numbers

---
+++ Day3/assignment25.js
@@ -0,0 +1,9 @@
+//WAP to mul two positive numbers ;
+
+const readlinesync = require ("readline-sync")
+
+let number1 = readlinesync.questionInt("enter number 1..?")
+let number2 = readlinesync.questionInt("enter number 2..?")
+let multiplication = number1 * number2;
+
+console.log(" multiplication of "+number1+  " and " +number2+  " is " + multiplication)
