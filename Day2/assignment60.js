4fc9b265052959ff962f5a48350d4d9e94bde2f6
shashi <shashichintu68@gmail.com>
2024-05-14 11:08:13 +0530

    FB profile in readable form, seperate by commas

---
+++ Day2/assignment60.js
@@ -0,0 +1,7 @@
+//WAP to print your FB profile in readable form, seperate by commas ( "Name : Hari, City : Hyderbad, No Of friends : 200 ....... ")
+
+let name = "Hari";
+let city = "hyd";
+let noOfFriends= 10;
+
+console.log("Name : "+name +", city : "+city+", No of friends : "+ noOfFriends)
