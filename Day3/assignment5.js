40394c4cac395d05362174eb6aa6e97240b8b269
shashi <shashichintu68@gmail.com>
2024-05-14 15:21:09 +0530

    declare variables  with null datatype

---
+++ Day3/assignment5.js
@@ -0,0 +1,7 @@
+//WAP to declare variables  with null datatype;
+
+const readlinesync = require ("readline-sync")
+
+let question1 = readlinesync.question("null");
+
+console.log(" null (" + question1+")")
