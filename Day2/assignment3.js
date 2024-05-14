3dc7f28f75e0912e85555ad3c8aba22aca7fc25c
shashi <shashichintu68@gmail.com>
2024-05-14 07:51:22 +0530

    declare variables  with boolean datatype

---
+++ Day2/assignment3.js
@@ -0,0 +1,12 @@
+//WAP to declare variables  with boolean datatype;
+
+ i_had_iphone = "true";
+
+ i_doesnt_have_iphone = "false";
+
+ console.log(i_had_iphone);
+ console.log(i_doesnt_have_iphone);
+
+
+ 
+
