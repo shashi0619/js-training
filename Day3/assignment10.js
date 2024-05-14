6e34484390295ef8a35b0b95e46ba592861e6569
shashi <shashichintu68@gmail.com>
2024-05-14 17:17:51 +0530

    number data from your linkedIn profile

---
+++ Day3/assignment10.js
@@ -0,0 +1,18 @@
+//WAP identifying the number data from your linkedIn profile, declare variables and assign values. Print them profile
+
+
+const readlineSync = require('readline-sync');
+
+
+let connections = readlineSync.question('Enter the number of connections: ');
+let yearsOfExperience = readlineSync.question('Enter the number of years of experience: ');
+
+
+connections = (connections);
+yearsOfExperience = (yearsOfExperience);
+
+
+
+console.log("LinkedIn Profile Information:");
+console.log("Connections:", connections);
+console.log("Years of Experience:", yearsOfExperience);
