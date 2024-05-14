43bbfe6e65fabddcf52f004199df1795d343d807
shashi <shashichintu68@gmail.com>
2024-05-14 17:22:54 +0530

    add two number and show the result

---
+++ Day3/assignment12.js
@@ -0,0 +1,9 @@
+//WAP to add two number and show the result;
+
+const readlinesync = require ("readline-sync")
+
+let number1 = readlinesync.questionInt("enter number 1..?")
+let number2 = readlinesync.questionInt("enter number 2..?")
+let sum = number1 + number2;
+
+console.log(" sum of "+number1+  " and " +number2+  " is " + sum)
