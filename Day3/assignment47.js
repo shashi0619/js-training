9103bce80cf6ddb37c63bf0780c73adedd23cd15
shashi <shashichintu68@gmail.com>
2024-05-14 20:00:42 +0530

    result of AND operation btween two boolean(true, true) values

---
+++ Day3/assignment47.js
@@ -0,0 +1,12 @@
+
+//WAP to show the result of AND operation btween two boolean(true, true) values
+
+const readlineSync = require('readline-sync');
+
+let booleanValue1 = readlineSync.question('Enter the first boolean value (true or false): ');
+
+let booleanValue2 = readlineSync.question('Enter the second boolean value (true or false): ');
+
+let result = booleanValue1 && booleanValue2;
+
+console.log("Result of AND operation:", result);
