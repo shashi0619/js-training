cd685a5e37ce78eca9adf85753429a8b818b376a
shashi <shashichintu68@gmail.com>
2024-05-14 08:11:25 +0530

    the boolean data from your facebook profile

---
+++ Day2/assignment8.js
@@ -0,0 +1,8 @@
+//WAP identifying the boolean data from your facebook profile, declare variables and assign values. Print them profile
+
+let i_had_fb = "true";
+
+let i_dont_have_fb = "false";
+
+console.log(i_had_fb);
+console.log(i_dont_have_fb);
