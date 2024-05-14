0a0ae1602c9ad1441ccdda3e4352ad0689ab5d15
shashi <shashichintu68@gmail.com>
2024-05-14 15:19:30 +0530

    declare variables  with undefined datatype

---
+++ Day3/assignment4.js
@@ -0,0 +1,7 @@
+//WAP to declare variables  with undefined datatype;
+
+const readlinesync = require ("readline-sync")
+
+let question1 = readlinesync.question("undefined");
+
+console.log(" undefined (" + question1+")")
