5636cef62ec87589fc7437ad99b3de7643fad778
shashi <shashichintu68@gmail.com>
2024-05-14 08:21:53 +0530

    the boolean data from your linkedIn profile

---
+++ Day2/assignment11.js
@@ -0,0 +1,8 @@
+//WAP identifying the boolean data from your linkedIn profile, declare variables and assign values. Print them profile
+
+let i_had_linkedin_connections = "true";
+
+let i_dont_have_linkedin_connections = "false";
+
+console.log(i_had_linkedin_connections);
+console.log(i_dont_have_linkedin_connections);
