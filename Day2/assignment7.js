d43f4e4a2d7e7fb7f3d278860d1d3d71d3776f25
shashi <shashichintu68@gmail.com>
2024-05-14 08:08:40 +0530

    the number data from your facebook profile

---
+++ Day2/assignment7.js
@@ -0,0 +1,13 @@
+//WAP identifying the number data from your facebook profile, declare variables and assign values. Print them profile
+
+let age = 10;
+number_of_friends = 200;
+number_of_posts = 50;
+number_of_likes = 50;
+number_of_comments = 50;
+
+console.log(age);
+console.log(number_of_friends);
+console.log(number_of_posts);
+console.log(number_of_likes);
+console.log(number_of_comments);
