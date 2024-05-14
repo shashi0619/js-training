7df92203f1026dea6917f6bfb701bf958e15f290
shashi <shashichintu68@gmail.com>
2024-05-14 18:25:17 +0530

    create two string and print it

---
+++ Day3/assignment38.js
@@ -0,0 +1,14 @@
+//WAP to create two string and print it
+
+const readlineSync = require('readline-sync');
+
+
+let firstString = readlineSync.question('Enter the first string: ');
+
+
+let secondString = readlineSync.question('Enter the second string: ');
+
+console.log('First string:', firstString);
+
+
+console.log('Second string:', secondString);
