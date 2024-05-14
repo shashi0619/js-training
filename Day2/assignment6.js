632a9fd6f512a0ed18e928d00e333916b56b1a5c
shashi <shashichintu68@gmail.com>
2024-05-14 08:05:06 +0530

    the string data from your facebook profile

---
+++ Day2/assignment6.js
@@ -0,0 +1,18 @@
+//WAP identifying the string data from your facebook profile, declare variables and assign values. Print them profile
+
+let name = " shashi";
+let mail = "shashixxx@mail.com";
+city = "hyderabad";
+gender = "male";
+DoB = "1993";
+user_name = "shashi1";
+password = "12345678";
+
+
+console.log(name);
+console.log(mail);
+console.log(city);
+console.log(gender);
+console.log(DoB);
+console.log(user_name);
+console.log(password);
