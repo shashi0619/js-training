b5606eee3858a9cb74cec2d60e01380913cac279
shashi <shashichintu68@gmail.com>
2024-05-14 11:07:10 +0530

    "Virat" and "Kohli" to get "Virat Kohli";

---
+++ Day2/assignment59.js
@@ -0,0 +1,8 @@
+//WAP to add "Virat" and "Kohli" to get "Virat Kohli"; (mind the space in the result);
+
+let name1 = "virat";
+let name3 = "kohli";
+
+let add = name1+ " " +name3;
+
+console.log(add)
