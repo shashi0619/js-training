0986bbda43e51d06b4f57c802d82ae716ae0b35c
shashi <shashichintu68@gmail.com>
2024-05-14 19:59:38 +0530

    result of OR operation btween two boolean(false, false) values

---
+++ Day3/assignment46.js
@@ -0,0 +1,11 @@
+//WAP to show the result of OR operation btween two boolean(false, false) values
+
+const readlineSync = require ("readline-sync");
+
+let booleanValue1 = readlineSync.question ("enter the first boolean Value");
+
+let booleanValue2 = readlineSync.question ("enter the second boolean value");
+
+let result = booleanValue1 || booleanValue2;
+
+console.log("Result of OR operation:", result);
