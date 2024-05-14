6132a84c2a39a1963e87dc1bba280921763cad81
shashi <shashichintu68@gmail.com>
2024-05-14 12:00:19 +0530

    declare variables  with number datatype

---
+++ Day3/assignment2.js
@@ -0,0 +1,7 @@
+//WAP to declare variables  with number datatype;
+
+const readlinesync = require ("readline-sync");
+
+let shashi = readlinesync.questionInt(" what is your age.? ");
+
+console.log( " your age "+shashi )
