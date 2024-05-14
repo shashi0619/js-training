b434e8ffbcec036bf9dc132a07379fbeb5421441
shashi <shashichintu68@gmail.com>
2024-05-14 11:10:10 +0530

    Linked profile in readable form, seperate by commas

---
+++ Day2/assignment61.js
@@ -0,0 +1,12 @@
+//WAP to print your Linked profile in readable form, seperate by commas ( "Name : Hari, City : Hyderbad, office : YiConnect .......")
+
+
+let profile ={ 
+    Name : "Hari",
+ 
+   city : "hyderabad",
+ office : "YiConnect..",
+};
+
+
+console.log(profile)
