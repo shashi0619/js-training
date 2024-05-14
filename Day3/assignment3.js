5c183383a97b9ab0f6a5eab0d106d3da19d15412
shashi <shashichintu68@gmail.com>
2024-05-14 15:17:09 +0530

    declare variables  with boolean datatype

---
+++ Day3/assignment3.js
@@ -0,0 +1,7 @@
+//WAP to declare variables  with boolean datatype;
+
+const readlinesync = require ("readline-sync");
+
+let question = readlinesync.question( " i have job " );
+
+console.log("i have a job "+ question  );
