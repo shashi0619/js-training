0709a3e70af95d62ee1389de0820c715d93851f5
shashi <shashichintu68@gmail.com>
2024-05-14 18:06:51 +0530

    sub one negative and one postive number

---
+++ Day3/assignment24.js
@@ -0,0 +1,10 @@
+//WAP to sub one negative and one postive number
+
+const readlinesync = require ("readline-sync")
+
+let number1 = readlinesync.questionInt("enter number 1...?")
+let number2 = readlinesync.questionInt("enter number 2....?")
+let sum = number1 - number2 ;
+
+
+console.log(" sum of "+number1+ " and " +number2+ " is "+sum)
