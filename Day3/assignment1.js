829e9a8f9f2b355f89be4b2254f33ba347b043e8
shashi <shashichintu68@gmail.com>
2024-05-14 11:19:25 +0530

    declare variables  with string datatype

---
+++ Day3/assignment1.js
@@ -0,0 +1,7 @@
+//WAP to declare variables  with string datatype
+
+const readlinesync = require ("readline-sync");
+
+let name = readlinesync.question("what is your name ? ");
+
+console.log("your name is "+name)
