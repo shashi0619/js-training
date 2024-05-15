e2d0805ead376324d0e23ace0971817ccff6d62a
shashi <shashichintu68@gmail.com>
2024-05-15 11:33:11 +0530

    find the maximum of two numbers by declaring them in code

---
+++ Day5/assignment4.js
@@ -0,0 +1,15 @@
+//WAP to find the maximum of two numbers by declaring them in code
+
+const readlineSync = require("readline-sync");
+
+const number1 = 50;
+const number2 = 20;
+
+let maximum;
+if (number1 >= number2) {
+    console.log ( number1 + "  number 1  is maximum "  );
+} else {
+    console.log ( number2 + "  number 2  is maximum "  );
+}
+
+console.log (" end of if else ")
