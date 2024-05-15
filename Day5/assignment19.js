fea03016bb2c69ae60d8e25fbc2d4d0dfef82834
shashi <shashichintu68@gmail.com>
2024-05-15 18:12:00 +0530

    two footbal scores

---
+++ Day5/assignment19.js
@@ -0,0 +1,28 @@
+//Given two footbal scores , find the winning team
+
+const readlineSync = require("readline-sync");
+
+const scoreTeam1 = readlineSync.question("Enter the score of Team 1: ");
+const scoreTeam2 = readlineSync.question("Enter the score of Team 2: ");
+
+
+let winningTeam;
+
+
+if (scoreTeam1 > scoreTeam2) 
+    {
+    winningTeam = "Team 1";
+}
+ else 
+    {
+    winningTeam = "Team 2";
+    }
+
+
+
+console.log("The winning team is: " + winningTeam);
+
+console.log (" end of if else ")
+
+
+
