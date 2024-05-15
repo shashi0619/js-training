9f19321a3869c442a177191b4fcb709c47b4ad87
shashi <shashichintu68@gmail.com>
2024-05-15 16:36:36 +0530

    the age of two brother

---
+++ Day5/assignment12.js
@@ -0,0 +1,23 @@
+//Given the age of two brother  WAP to find who is elder
+
+const readlineSync = require("readline-sync");
+
+
+const brother1 = readlineSync.question("Enter the age of the first brother: ");
+
+const brother2 = readlineSync.question("Enter the age of the second brother: ");
+
+
+if (brother1 > brother2) 
+    
+    {
+    console.log("The first brother is older.");
+
+}
+else
+  { console.log (" The second brother is older." );
+
+
+  }
+
+  console.log (" end of if else ")
