0c546172eaa426d0c8b475d959f6343050d93234
shashi <shashichintu68@gmail.com>
2024-05-15 11:44:34 +0530

    find the minimum of two numbers  by declaring them in code

---
+++ Day5/assignment5.js
@@ -0,0 +1,16 @@
+//WAP to find the minimum of two numbers  by declaring them in code
+
+
+const readlineSync = require("readline-sync");
+
+const number1 = 50;
+const number2 = 20;
+
+let minimum;
+if (number1 <= number2) {
+    console.log ( number1 + "  number 1  is minimum "  );
+} else {
+    console.log ( number2 + "  number 2  is minimum "  );
+}
+
+console.log (" end of if else ")
