d519f4de2e6ed463c40c4cd0edcea63ac949e4b5
shashi <shashichintu68@gmail.com>
2024-05-15 10:56:50 +0530

    find max num by if else

---
+++ Day4/assignment28.js
@@ -0,0 +1,14 @@
+//WAP to calculate the total score of a cricket team in an inning
+
+
+
+const readlineSync = require('readline-sync');
+
+
+let totalPlayers = readlineSync.questionInt(readlineSync.question('Enter the total number of players: '));
+
+let totalScore = 0;
+
+
+
+
---
+++ Day5/assignment2.js
@@ -0,0 +1,21 @@
+// wap to find max num by if else 
+
+const number1 = 100;
+
+const number2 = 200;
+
+const result = (number1 <= number2)
+
+if (number1>=number2)
+    {
+       console.log (number1 + "  number1 is maximum " )
+    }
+
+ else   
+ {
+    console.log ( number2 + " number2 is maximum")
+ } 
+
+ console.log (" end of if else ")
+
+
