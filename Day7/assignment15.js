d82764f13db2ebdba8e7f42cf684b4331d90b4cc
shashi <shashichintu68@gmail.com>
2024-05-17 17:25:11 +0530

    find the largest of the 4 numbers

---
+++ Day7/assignment15.js
@@ -0,0 +1,21 @@
+//WAP to find the largest of the 4 numbers
+
+const readlineSync = require("readline-sync");
+
+const num1 = readlineSync.question("Enter number 1: ");
+const num2 = readlineSync.question("Enter number 2: ");
+const num3 = readlineSync.question("Enter number 3: ");
+const num4 = readlineSync.question("Enter number 4: ");
+
+if (num1 >= num2 && num1 >= num3 && num1 >= num4) {
+    console.log("The largest of the 4 numbers is: " + num1);
+}
+if (num2 >= num1 && num2 >= num3 && num2 >= num4) {
+    console.log("The largest of the 4 numbers is: " + num2);
+}
+if (num3 >= num1 && num3 >= num2 && num3 >= num4) {
+    console.log("The largest of the 4 numbers is: " + num3);
+}
+if (num4 >= num1 && num4 >= num2 && num4 >= num3) {
+    console.log("The largest of the 4 numbers is: " + num4);
+}
