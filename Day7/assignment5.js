efd920fda306e8affb31aed069813381fa3e1a78
shashi <shashichintu68@gmail.com>
2024-05-17 12:13:57 +0530

    find the if three number are equal

--- Day7/assignment5.js
+++ Day7/assignment5.js
@@ -1,1 +1,17 @@
 //WAP to find the if three number are equal
+
+const readline = require('readline-sync');
+
+const number1 = readline.question("Enter the first number: ");
+const number2 = readline.question("Enter the second number: ");
+const number3 = readline.question("Enter the third number: ");
+
+if (number1 === number2) {
+    if (number1 === number3) {
+        console.log("All three numbers are equal.");
+    }
+}
+
+if (number1 != number2 || number1 != number3) {
+    console.log("The numbers are not equal.");
+}
