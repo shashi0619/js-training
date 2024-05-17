947779bcd9e2d9ef932ce202f9a2c63ebc6dfc79
shashi <shashichintu68@gmail.com>
2024-05-17 12:49:26 +0530

    the sides of three squares

---
+++ Day7/assignment7.js
@@ -0,0 +1,22 @@
+//Given the sides of three squares WAP to find the square with maximum area
+
+const readlineSync = require("readline-sync");
+
+
+const sideLength1 = readlineSync.question("Enter the side length of the first square: ");
+const sideLength2 = readlineSync.question("Enter the side length of the second square: ");
+const sideLength3 = readlineSync.question("Enter the side length of the third square: ");
+
+const area1 = sideLength1 * sideLength1;
+const area2 = sideLength2 * sideLength2;
+const area3 = sideLength3 * sideLength3;
+
+if (area1 > area2 && area1 > area3) {
+    console.log("The first square  has the maximum area: " + area1);
+}
+if (area2 > area1 && area2 > area3) {
+    console.log("The second square  has the maximum area: " + area2);
+}
+if (area3 > area1 && area3 > area2) {
+    console.log("The third square  has the maximum area: " + area3);
+}
